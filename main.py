614a4c5266ebdc4c3c3e16a71c759ed17a35e2af
osiPete <osiPete@users.noreply.github.com>
2019-05-01 16:38:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from flask import Flask
-from serve import useless_function  
+from flashtext.keyword import KeywordProcessor
+import pickle
 
-app=Flask(__name__)
+# Function that takes loads in our pickled word processor
+# and defines a function for using it. This makes it easy
+# to do these steps together when serving our model.
+def get_keywords_api():
+    
+    # read in pickled word processor. You could also load in
+    # other models as this step.
+    keyword_processor = pickle.load(open("processor.pkl", "rb"))
+    
+    # Function to apply our model & extract keywords from a 
+    # provided bit of text
+    def keywords_api(text, span_info=True): 
+        keywords_found = keyword_processor.extract_keywords(text, span_info=True)      
+        return keywords_found
+    
+    # return the function we just defined
+    return keywords_api
 
-# Define our "ping" end point
-@app.route('/ping')
-def useless_output():
-  return(useless_function())
