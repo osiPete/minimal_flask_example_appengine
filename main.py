7987773e10e6598cb03708bcb549119bccd87d8b
osiPete <osiPete@users.noreply.github.com>
2019-05-01 12:30:02 -0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from serve import useless_function  
-
-app=Flask(__name__)
-
-# Define our "ping" end point
-@app.route('/ping')
-def useless_output():
-  return(useless_function())
