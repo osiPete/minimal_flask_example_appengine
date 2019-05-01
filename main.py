48768988894f6cc8768c17dadb7a43088ddf4af9
osiPete <osiPete@users.noreply.github.com>
2019-05-01 16:48:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
-from flashtext.keyword import KeywordProcessor
-import pickle
+from flask import Flask, request
+from serve import get_keywords_api
+import json
+# I've commented out the last import because it won't work in kernels, 
+# but you should uncomment it when we build our app tomorrow
 
-# Function that takes loads in our pickled word processor
-# and defines a function for using it. This makes it easy
-# to do these steps together when serving our model.
-def get_keywords_api():
-    
-    # read in pickled word processor. You could also load in
-    # other models as this step.
-    keyword_processor = pickle.load(open("processor.pkl", "rb"))
-    
-    # Function to apply our model & extract keywords from a 
-    # provided bit of text
-    def keywords_api(text, span_info=True): 
-        keywords_found = keyword_processor.extract_keywords(text, span_info=True)      
-        return keywords_found
-    
-    # return the function we just defined
-    return keywords_api
+# create an instance of Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Up and running!"
+
+# load our pre-trained model & function
+keywords_api = get_keywords_api()
+
+# Define a post method for our API.
+@app.route('/extractpackages', methods=['POST'])
+def extractpackages():
+    """ 
+    Takes in a json file, extracts the keywords &
+    their indices and then returns them as a json file.
+    """
+    # the data the user input, in json format
+    #input_data = request.json
+    input_data = request.get_json()
+
+    # use our API function to get the keywords
+    output_data = keywords_api(input_data)
+    #output_data = input_data
+
+    # convert our dictionary into a .json file
+    # (returning a dictionary wouldn't be very
+    # helpful for someone querying our API from
+    # java; JSON is more flexible/portable)
+    response = json.dumps(output_data)
+    #response = input_data
+
+    # return our json file
+    return response
